6eec152d4b7bbd81f0ae584534909e63cc56915d
Rebecca Valentine <beka@gothdyke.mom>
2021-03-09 12:52:40 -0800

    Reorgs testing code to be more generic

--- tests/helpers.py
+++ tests/helpers.py
@@ -21,3 +21,39 @@ def comment_line(line):
 
 def blank_line(line):
     return re.match('^\s*$', line) is not None
+
+
+def test_modules(modules, positives, negatives):
+
+    for mod in modules:
+
+        print()
+        print(mod.__name__ + ':')
+
+        print()
+        false_neg_errors = []
+        for (lino, line) in positives:
+            found = mod.locate(line)
+            if not found:
+                false_neg_errors.append((lino, line))
+
+        if not false_neg_errors:
+            print('  No false negatives.')
+        else:
+            print('  Found false negatives:')
+            for lino, line in false_neg_errors:
+                print('    line %i  --  %s' % (lino, line))
+
+        print()
+        false_pos_errors = []
+        for (lino, line) in negatives:
+            found = mod.locate(line)
+            if found:
+                false_pos_errors.append((lino, line))
+
+        if not false_pos_errors:
+            print('  No false positives.')
+        else:
+            print('  Found false positives:')
+            for lino, line in false_pos_errors:
+                print('    line %i  --  %s' % (lino, line))
